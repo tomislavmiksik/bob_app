054524a7a4749f89725d5270e2af2bd3aa67e2c9
Tomislav Miksik <tomislav.miksik@gmail.com>
2025-08-12 20:30:04 +0200

    feat: init commit

---
+++ apps/bob_be/bob_be_flutter/android/app/src/main/kotlin/com/example/bob_be_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bob_be_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/bob_fe/android/app/src/main/kotlin/com/example/bob_fe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bob_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
