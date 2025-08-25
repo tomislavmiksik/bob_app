8d174124bae8e6a8d35c5ba4795cc8384bdea439
Tomislav Miksik <tomislav.miksik@gmail.com>
2025-08-25 13:19:53 +0200

    fix: export paths

--- packages/models/lib/bob_models.dart
+++ packages/models/lib/bob_models.dart
@@ -1,6 +1,6 @@
 library bob_models;
 
-export 'package:bob_be_server/lib/src/generated/endpoints.dart';
-export 'package:bob_be_server/lib/src/generated/classes/cicd_workflow_run.dart';
-export 'package:bob_be_server/lib/src/generated/enums/cicd_workflow_platform.dart';
-export 'package:bob_be_server/lib/src/generated/greeting.dart';
+export 'package:bob_be_server/src/generated/endpoints.dart';
+export 'package:bob_be_server/src/generated/classes/cicd_workflow_run.dart';
+export 'package:bob_be_server/src/generated/enums/cicd_workflow_platform.dart';
+export 'package:bob_be_server/src/generated/protocol.dart';
